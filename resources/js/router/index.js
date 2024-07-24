c9157e357cea39a2c197babba12c97fb697927e5
Mahmut İÇME <arrovain@gmail.com>
2024-07-24 10:39:26 +0300

    routes array

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,8 +1,19 @@
-import { createRouter, createWebHashHistory} from "vue-router";
+import { createRouter, createWebHistory} from "vue-router";
+
+const routes = [
+    {
+        path: '/',
+        name: 'products.index',
+        component: productIndex
+    }
+
+]
+
 
 const router = createRouter({
     history: createWebHistory(),
-    
+    rou
+
 });
 
 export default router;
