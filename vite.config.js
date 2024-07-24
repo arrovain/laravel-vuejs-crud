de2bd97499a449ab67cfcda779e14c749bfbf542
Mahmut İÇME <arrovain@gmail.com>
2024-07-24 10:32:01 +0300

    add vue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vite/plugin-vue';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,5 +8,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+
+        vue()
     ],
 });
