c3210a82b01e0ee5f2ac65f620752f05fac75422
Mahmut İÇME <arrovain@gmail.com>
2024-07-24 10:43:55 +0300

    use Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import './bootstrap';
 
 import { createApp } from 'vue';
-
+import router from './router';
 import app from './components/App.vue'
 
-createApp(app).mount("#app")
+createApp(app).use(router).mount("#app")
